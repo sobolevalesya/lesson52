8be987f335fa55b48973a6fe3d3ede1fd3b71284
lesyafedchenko <lesyasobolevaa@gmail.com>
2023-11-28 02:06:43 +0100

    Настроила админку

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from todo.models import Task
 
-# Register your models here.
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'status', 'deadline']
+
